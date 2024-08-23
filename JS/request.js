b41d8ec747cb2edef1d5c9e7feef063d4f12132d
ItzSanuka <ItzSanuka@outlook.com>
2024-08-23 22:49:28 +1000

    Added requests functionally

--- JS/request.js
+++ JS/request.js
@@ -7,7 +7,9 @@ function Request(firstName, lastName, email, phoneNo, address, designs) {
     this.designs = designs;
 }
 
-document.getElementById('submit').onclick = async function() {
+document.getElementById('form').onsubmit = async function(event) {
+    event.preventDefault();
+    
     const person = {
         firstName: document.getElementById('firstName').value,
         lastName: document.getElementById('lastName').value
@@ -21,5 +23,25 @@ document.getElementById('submit').onclick = async function() {
 
     const newRequest = new Request(person.firstName, person.lastName, contact.email, contact.phoneNo, address, design);
 
-    alert(`Hello ${person.firstName}, your request has been sent to us. We will contact you shortly.`);
+    try {
+        const response = await fetch('https://buildup-backend.onrender.com/requests/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newRequest)
+        });
+
+        if (response.ok)
+            alert(`Hello ${person.firstName}, your request has been sent to us. We will contact you shortly.`);
+        else {
+            const errorMessage = await response.text();
+            throw new Error(`Server responded with status ${response.status}: ${errorMessage}`);
+        }
+    } catch (err) {
+        console.error("Error when sending request to back-end:", err.message);
+        alert(`Hello ${person.firstName}, there was an error when sending your request. Please try again later`);
+    }
+
+    location.href = "../index.html";
 };
